2e78f4761dbd64ccfaa28a34c354c79591c80316
Administrator <Administrator@i7E-13>
2025-02-25 17:51:05 +0900

    d

---
+++ src/com/test/dto/FoodInfoDTO.java
@@ -0,0 +1,31 @@
+package com.test.dto;
+
+public class FoodInfoDTO {
+	private int fiNum;
+	private String fiName;
+	private int fiPrice;
+	public int getFiNum() {
+		return fiNum;
+	}
+	public void setFiNum(int fiNum) {
+		this.fiNum = fiNum;
+	}
+	public String getFiName() {
+		return fiName;
+	}
+	public void setFiName(String fiName) {
+		this.fiName = fiName;
+	}
+	public int getFiPrice() {
+		return fiPrice;
+	}
+	public void setFiPrice(int fiPrice) {
+		this.fiPrice = fiPrice;
+	}
+	@Override
+	public String toString() {
+		return "FoodInfoDTO [fiNum=" + fiNum + ", fiName=" + fiName + ", fiPrice=" + fiPrice + "]";
+	}
+	
+	
+}
